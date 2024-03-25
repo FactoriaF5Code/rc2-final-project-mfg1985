aae903de79269ad2706443e8999256434fe75e4a
mfg1985 <mfg1985@me.com>
2024-03-26 00:13:32 +0100

    Card Recipe

---
+++ frontend/src/presentation/components/CardRecipe/CardRecipe.jsx
@@ -0,0 +1,14 @@
+import "./CardRecipe.css";
+
+export const CardRecipe = () => {
+  return (
+    <div className="card">
+      <div className="titulo">
+        <h2>Titulo receta</h2>
+      </div>
+      <div className="photo">
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
--- frontend/src/presentation/pages/HomePage.jsx
+++ frontend/src/presentation/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header/Header";
+import { CardRecipe } from "../components/CardRecipe/CardRecipe";
 
 export const HomePage = () => {
   return (
     <>
       <body>
         <Header />
+        <CardRecipe />
         <Outlet />
       </body>
     </>
