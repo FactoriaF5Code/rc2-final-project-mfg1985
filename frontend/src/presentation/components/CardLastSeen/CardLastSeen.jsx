3aa1c740f2049a26bcdef6c29574ac0d6439067a
mfg1985 <mfg1985@me.com>
2024-03-26 11:55:50 +0100

    CardLastSeen

---
+++ frontend/src/presentation/components/CardLastSeen/CardLastSeen.jsx
@@ -0,0 +1,13 @@
+ import "./CardLastSeen.css";
+
+export const CardLastSeen = () => {
+    return (
+<>
+<div className="cardLastSeen">
+    <img className="imgLastSeen" src="" alt="" />
+    <h2>Titulo Receta</h2>
+    
+</div>
+</>
+    );
+};
