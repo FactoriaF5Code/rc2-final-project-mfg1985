2466403e4fd3e602dfd80c7cb02ba59a2cb2618b
mfg1985 <mfg1985@me.com>
2024-03-29 17:29:07 +0100

    format form

--- frontend/src/presentation/components/NewRecipe/NewRecipe.jsx
+++ frontend/src/presentation/components/NewRecipe/NewRecipe.jsx
@@ -1,55 +1,67 @@
-import "./NewRecipe.css"
+import "./NewRecipe.css";
 
 export const NewRecipe = () => {
   return (
-
-  <section className="newrecipe-conteiner">
-    <form>
-        {/* titulo */}
-        <label htmlFor="titulo">Titulo</label>
-        <input className="title" type="text"  />
-
+    <section className="newrecipe-conteiner">
+      <form>
+        <div className="title-conteiner">
+          {/* titulo */}
+          <label htmlFor="titulo">Titulo</label>
+          <input  className="form-title" type="text" />
+        </div>
+        <div className="time-conteiner">
         {/* tiempo */}
         <label htmlFor="time">Tiempo</label>
-        <input className="time" type="text" />
-
+        <input className="form-time"  type="text" />
+        </div>
+        <div className="servings-conteiner">
         {/* Raciones */}
         <label htmlFor="servings">Raciones</label>
-        <input className="se" type="text" />
-
+        <input className="form-servings" type="text" />
+        </div>
+        <div className="tags-conteiner">
         {/* etiquetas */}
         <label htmlFor="tags">Etiquetas</label>
-        <input type="text" />
-
+        <input className="form-tags" type="text" />
+        </div>
+        <div className="dishtype-conteiner">
         {/* plato */}
         <label htmlFor="dish_type">Plato</label>
-        <input type="text" />
-
+        <input  className="form-dishtype" type="text" />
+        </div>
+        <div className="collection-conteiner">
         {/* coleccion */}
         <label htmlFor="collection">Colección</label>
-        <input type="text" />
-
+        <input className="form-collection" type="text" />
+        </div>
+        <div  className="category-conteiner">
         {/* categoria */}
         <label htmlFor="category">Categoria</label>
-        <input type="text" />
-
+        <input className="form-category" type="text" />
+        </div>
+        <div className="ingredients-conteiner">
         {/* ingredientes */}
         <label htmlFor="ingredients">Ingredientes</label>
-        <input type="text" />
-
+        <input className="form-ingredients" type="text" />
+        </div>
+        <div className="instructions-conteiner">
         {/* instruciones */}
         <label htmlFor="instructions">Paso a paso</label>
-        <input type="text" />
-
+        <input className="form-isntruccion"type="text" />
+        </div>
+        <div className="images-conteiner">
         {/* imagenes */}
         <label htmlFor="images">Imagenes</label>
-        <input type="text" />
-
+        <input className="form-images"type="text" />
+        </div>
+        <div className="notes-conteiner">
         {/* notas */}
-        <label htmlFor="note">Notas</label>
-        <input type="text" />
-
-    </form>
-  </section>
-);
+        <label htmlFor="notes">Notas</label>
+        <input className="form-notes" type="text" />
+        </div>
+        <button className="save-recipe">Guardar</button>
+      </form>
+    
+    </section>
+  );
 };
